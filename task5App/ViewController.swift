b27b859cf340293e4701c81aa0ef0383565de8d0
yasu1125 <v22.m1125@gmail.com>
2021-03-28 15:10:03 +0900

    Initial Commit

---
+++ task5App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task5App
+//
+//  Created by yasudamasato on 2021/03/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
